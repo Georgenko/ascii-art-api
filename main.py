db0ab5c35acd1f5ab882ff2c31695b803e4e929a
Georgenko <g.stanimirov.m@gmail.com>
2026-03-25 16:20:18 +0200

    Adds endpoint for text-to-banner to generate banner with Pyfiglet

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+from pydantic import BaseModel
+import pyfiglet
+
+class TextToBanner(BaseModel):
+	prompt: str
+	font: str | None = None
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/text-to-banner", response_class=PlainTextResponse)
+async def text_to_banner(text_to_banner: TextToBanner):
+	# TODO: add validation for fonts, if they are not one of the supported for pyfiglet, it crashes
+	f = pyfiglet.figlet_format(text_to_banner.prompt, font=text_to_banner.font)
+	return f
