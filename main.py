90642e430ba5ecd3882c9a08bf28c3c5896e9375
Georgenko <g.stanimirov.m@gmail.com>
2026-03-27 12:27:59 +0200

    Adds validation for cyrillic/latin fonts and prompts

--- main.py
+++ main.py
@@ -1,24 +1,67 @@
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
-from pydantic import BaseModel, AfterValidator
-from typing import Annotated
+from pydantic import BaseModel, model_validator
 import pyfiglet
+import unicodedata as ud
 
-available_fonts = pyfiglet.Figlet().getFonts()
 
-def check_valid_font(font: str):
-	if font not in available_fonts:
-		raise ValueError(f"Invalid font: {font}. Here is the list of supported ones: {available_fonts}")
-	return font
+CYRILLIC = 'CYRILLIC'
+LATIN = 'LATIN'
+CYRILLIC_DISPLAY = "Cyrillic"
+LATIN_DISPLAY = "Latin"
+all_available_fonts = pyfiglet.Figlet().getFonts()
+app = FastAPI()
+
+
+def get_cyrillic_fonts():
+	dummy_input = "Здравейте"
+	working_fonts = []
+
+	for font in all_available_fonts:
+		dummy_output = pyfiglet.figlet_format(dummy_input, font=font)
+		if dummy_output.strip():
+			working_fonts.append(font)
+	
+	return working_fonts
+
+cyrillic_fonts = get_cyrillic_fonts()
+
 
 class TextToBanner(BaseModel):
 	prompt: str
-	font: Annotated[str, AfterValidator(check_valid_font)] = pyfiglet.DEFAULT_FONT
+	font: str = pyfiglet.DEFAULT_FONT
+	cyrillic: bool = False
 
-app = FastAPI()
+	@model_validator(mode='after')
+	def validate(self):
+
+		# validate font
+		valid_fonts = cyrillic_fonts if self.cyrillic else all_available_fonts
+		if self.font not in valid_fonts:
+			raise ValueError(f"Invalid font: {self.font}. The available {CYRILLIC_DISPLAY if self.cyrillic else ''} fonts are: {valid_fonts}")
+
+		# validate prompt
+		for char in self.prompt:
+			if char.isspace():
+				continue
+
+			category = ud.category(char)
+
+			if not category.startswith(('L', 'N', 'P', 'S')):
+				raise ValueError(f"Invalid char: {char}. Category {category} found. Only letters, digits, punctuation, and symbols are allowed.")
+
+			if category.startswith('L'):
+				expected_script = CYRILLIC if self.cyrillic else LATIN
+				char_name = ud.name(char)
+
+				if expected_script not in char_name:
+					script_type = CYRILLIC_DISPLAY if self.cyrillic else LATIN_DISPLAY
+					raise ValueError(f"Invalid char: {char}. Only {script_type} letters allowed (along with digits, punctuation, and symbols)")
+
+		return self
+
 
 @app.post("/text-to-banner", response_class = PlainTextResponse)
 async def text_to_banner(text_to_banner: TextToBanner):
-	# TODO: add validation for prompt text as well (no cyrillic, maybe length and so on)
-	f = pyfiglet.figlet_format(text_to_banner.prompt, font=text_to_banner.font)
-	return f
+	banner = pyfiglet.figlet_format(text_to_banner.prompt, font=text_to_banner.font)
+	return f"Font: {text_to_banner.font}\n\n{banner}"
