fc53493c692c6aa614c3ea1ca0e0b8cacee862bc
Georgenko <g.stanimirov.m@gmail.com>
2026-04-10 12:04:09 +0300

    Displays result of text-to-banner request

--- static/js/banner.js
+++ static/js/banner.js
@@ -1,15 +1,19 @@
-function buildBannerRequest(prompt, font = "standard", cyrillic = false) {
-    return {prompt, font, cyrillic};
-}
-
 async function convertTextToBanner() {
     const input = document.querySelector("#text-to-banner input");
     const request = buildBannerRequest(input.value);
 
     try {
         const banner = await postTextToBanner(request);
-        console.log(banner); // swap with DOM rendering later
+        showResult(banner);
     } catch (err) {
         console.error(err.message);
     }
+}
+
+function buildBannerRequest(prompt, font = "standard", cyrillic = false) {
+    return {prompt, font, cyrillic};
+}
+
+function showResult(text) {
+  document.getElementById("result").textContent = text;
 }
