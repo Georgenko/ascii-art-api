7e9867128ad1c471508e87a2f62aeaee2fca19e5
Georgenko <g.stanimirov.m@gmail.com>
2026-04-25 12:05:41 +0300

    Gets minimal from user for image-to-image

--- static/js/image.js
+++ static/js/image.js
@@ -2,6 +2,8 @@ async function convertImageToImage(){
     const fileInput = document.querySelector('#image-to-ascii input[type="file"]');
     const file = fileInput.files[0];
 
+    const minimal = document.getElementById("checkbox-minimal").checked;
+
     if (!file) {
         alert('Please select an image first');
         return;
@@ -10,7 +12,6 @@ async function convertImageToImage(){
     const formData = new FormData();
     formData.append('image', file);
 
-    const minimal = true;
     try { // TODO: add width input in html and get from user
         const imageResult = await postImageToImage(formData, minimal, 500)
         showResult(imageResult, "image-view");
